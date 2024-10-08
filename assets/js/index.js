c464c3f48a4ad7a6850ee244c016cf650a0c1546
PRATYUSH CHANDRA <159172039+pratyush-Chandra@users.noreply.github.com>
2024-10-09 01:25:24 +0530

    Add files via upload

--- assets/js/index.js
+++ assets/js/index.js
@@ -18,3 +18,40 @@ const gotoTopBtn = document.getElementById("goto-top");
 gotoTopBtn.addEventListener("click", () => {
     window.scrollTo({ top: 0, behavior: "smooth" });
 });
+
+window.onload = () => {
+    const skillsCardParent = document.getElementById("skills-content");
+    for (let i = 0; i < skillsCardParent.children.length; i++) {
+        if (i > 7) {
+            skillsCardParent.children[i].classList.add("hidden");
+        }
+    }
+    skillsCardParent.style.gridTemplateRows = "repeat(2, 1fr)";
+}
+
+const showMoreBtn = document.getElementById("show-more");
+localStorage.setItem("isVisisble", true);
+showMoreBtn.addEventListener("click", () => {
+    if (localStorage.getItem("isVisisble") === "true") {
+        const skillsCardParent = document.getElementById("skills-content");
+        skillsCardParent.style.gridTemplateRows = "repeat(4, 1fr)";
+        for (let i = 0; i < skillsCardParent.children.length; i++) {
+            if (i > 7) {
+                skillsCardParent.children[i].classList.remove("hidden");
+            }
+        }
+        showMoreBtn.innerHTML = `<span>View Less <i class="bx bx-up-arrow-alt"></i></span>`;
+        localStorage.setItem("isVisisble", false);
+    }
+    else {
+        const skillsCardParent = document.getElementById("skills-content");
+        for (let i = 0; i < skillsCardParent.children.length; i++) {
+            if (i > 7) {
+                skillsCardParent.children[i].classList.add("hidden");
+            }
+        }
+        skillsCardParent.style.gridTemplateRows = "repeat(2, 1fr)";
+        showMoreBtn.innerHTML = `<span>More Skills <i class="bx bx-down-arrow-alt"></i></span>`;
+        localStorage.setItem("isVisisble", true);
+    }
+})
